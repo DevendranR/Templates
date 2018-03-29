6945027f4843a9d46c34018ee06326db037f47fb
DevendranR <35388086+DevendranR@users.noreply.github.com>
2018-03-29 20:08:57 +0530

    Initial Commit
    InitialCommit

---
+++ UnZip.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class UnZip {
+	public static void unzip (String sourceFilePath,String destinationFolderPath) {
+
+		try {
+			
+			ZipFile zipFile = new ZipFile(sourceFilePath);
+			Enumeration<?> enu = zipFile.entries();
+			while (enu.hasMoreElements()) {
+				ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+				String name = zipEntry.getName();
+				long size = zipEntry.getSize();
+				long compressedSize = zipEntry.getCompressedSize();
+				
+				
+				if(!name.contains(".zip")){
+					throw new Exception(name+" project is not zipped ");
+				}
+				
+				File file = new File(destinationFolderPath+name);
+				if (name.endsWith("/")) {
+					file.mkdirs();
+					continue;
+				}
+
+				File parent = file.getParentFile();
+				if (parent != null) {
+					parent.mkdirs();
+				}
+
+				InputStream is = zipFile.getInputStream(zipEntry);
+				FileOutputStream fos = new FileOutputStream(file);
+
+				byte[] bytes = new byte[1024];
+				int length;
+				while ((length = is.read(bytes)) >= 0) {
+					fos.write(bytes, 0, length);
+				}
+				is.close();
+				fos.close();
+
+			}
+			zipFile.close();
+			
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
---
+++ Zip.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+		
+	
+	
+	
+	public static void getAllFiles(File dir, List<File> fileList) {
+			try {
+				File[] files = dir.listFiles();
+				for (File file : files) {
+					fileList.add(file);
+					if (file.isDirectory()) {
+						getAllFiles(file, fileList);
+					} else {
+						System.out.println("No Files in path");
+					}
+				}
+			} 
+			catch (Exception e) 
+			{
+				System.out.println(e.getMessage());
+			}
+		}
+	
+		public static void writeZipFile(String pathOfResultantZippedFiles,File directoryToZip) {
+	
+			try {
+				FileOutputStream fos = new FileOutputStream(pathOfResultantZippedFiles+directoryToZip.getName() + ".zip");
+				ZipOutputStream zos = new ZipOutputStream(fos);
+				
+				List<File> fileList = new ArrayList<File>();
+				getAllFiles(directoryToZip, fileList);
+				
+				for (File file : fileList) {
+					
+					if (!file.isDirectory()) { 
+						addToZip(directoryToZip, file, zos);
+					}
+				}
+	
+				zos.close();
+				fos.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	
+		public static void addToZip(File directoryToZip, File file, ZipOutputStream zos) {
+	
+			try {
+				
+				FileInputStream fis = new FileInputStream(file);
+		
+				// we want the zipEntry's path to be a relative path that is relative
+				// to the directory being zipped, so chop off the rest of the path
+				String zipFilePath = file.getCanonicalPath().substring(directoryToZip.getCanonicalPath().length() + 1,
+						file.getCanonicalPath().length());
+				
+					ZipEntry zipEntry = new ZipEntry(zipFilePath);
+					zos.putNextEntry(zipEntry);
+					IOUtils.copy(fis, zos);
+					zos.closeEntry();
+					fis.close();
+				
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} 
+		}
+}
